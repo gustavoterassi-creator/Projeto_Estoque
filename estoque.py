35c5c98ffb61e9d0bcb6a2eb7dbee340382df1e0
gustavoterassi-creator <gustavo.terassi@edu.senai.br>
2026-06-18 11:09:47 -0300

    Estou na ultima parte do codigo

---
+++ estoque.py
@@ -0,0 +1,62 @@
+estoqueP = [
+    [1,"Teclado",4,"Prateleira 01"],
+    [2,"Mouse",3,"Prateleira 02"],
+    [3,"Monitor",2,"Prateleira 03"],
+    [4,"Mouse pad",3,"Prateleira 04"]
+]
+for linha in estoqueP:
+    print(linha)
+
+
+def registrarProduto():
+    novoproduto = input("Qual o nome do novo produto?: ")
+    idProduto = input("Qual o id do produto: ")
+    quantidadesdp = input("Qual a quantidade dos produtos: ")
+    localizaçaop = input("Qual a localizaçao do pruduto:")
+    estoqueP.append([novoproduto,idProduto,quantidadesdp,localizaçaop])
+    print("Produto inserido com sucesso! ")
+
+def idProdutos():
+    linhaProcurada = -1
+    idProduto = int(input("Qual o id dos produtos:"))
+    for i in range(len(estoqueP)):
+        if(estoqueP[i][0] == idProdutos):
+            linhaProcurada = i
+    print(f"O nome procurado está na linha {linhaProcurada}")
+
+
+def listaP():
+    print("----- Produtos -----")
+    print(f"Temos {estoqueP} de estoque disponivel:")
+    print("--------------------------")
+
+def 
+
+while True:
+    print("\nBem vindo ao menu interativo do estoque. Por favor selecione uma opção:")
+    print("\n1- NovoProduto| 2-idProdutos | 3-lista produtos| 4- atualizarE | 5- Sair:")
+    opcao = input("Escolha: ")
+    if (opcao == "1"):
+        registrarProduto()
+    elif (opcao == "2"):
+        idProdutos()
+    elif (opcao =="3"):
+        listaP()
+    elif (opcao =="4"):()
+    
+       
+       
+
+
+    
+
+
+
+
+
+
+
+ 
+ 
+ 
+ 
